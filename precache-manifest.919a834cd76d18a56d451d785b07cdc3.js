c5c32020176bc55cdd3014c3f6f73e8d22ae2842
zawtun <zzt.nyc@gmail.com>
2020-08-08 00:14:20 -0400

    Updates

---
+++ precache-manifest.919a834cd76d18a56d451d785b07cdc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b70dda1454a87047dd29cf5bf53267",
+    "url": "/zawztun/youtube-video/index.html"
+  },
+  {
+    "revision": "5257b887333b1c0604cf",
+    "url": "/zawztun/youtube-video/static/css/main.2de54ffd.chunk.css"
+  },
+  {
+    "revision": "69bb0e27240900d37065",
+    "url": "/zawztun/youtube-video/static/js/2.aa78281f.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/zawztun/youtube-video/static/js/2.aa78281f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5257b887333b1c0604cf",
+    "url": "/zawztun/youtube-video/static/js/main.865b843a.chunk.js"
+  },
+  {
+    "revision": "b32b269e737ecf13f595",
+    "url": "/zawztun/youtube-video/static/js/runtime-main.f721fc4a.js"
+  }
+]);
