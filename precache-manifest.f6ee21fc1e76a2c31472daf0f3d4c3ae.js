f2c6bed1da109b51f1b0cafc4e6c5a8cea6c3274
zaw tun <zzt.nyc@gmail.com>
2022-05-26 11:41:23 -0400

    Updates

---
+++ precache-manifest.f6ee21fc1e76a2c31472daf0f3d4c3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff00c56896838057fd6119dc9fbb9964",
+    "url": "/youtube-video/index.html"
+  },
+  {
+    "revision": "caa38604ab8fe84b012e",
+    "url": "/youtube-video/static/css/main.ecdad606.chunk.css"
+  },
+  {
+    "revision": "e19ba056e06c2917120f",
+    "url": "/youtube-video/static/js/2.b8aa310c.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/youtube-video/static/js/2.b8aa310c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "caa38604ab8fe84b012e",
+    "url": "/youtube-video/static/js/main.44687f72.chunk.js"
+  },
+  {
+    "revision": "9abcf7e2ba842a018c6f",
+    "url": "/youtube-video/static/js/runtime-main.189be57a.js"
+  }
+]);
